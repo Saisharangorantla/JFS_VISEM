ed02057c77823b8206e7e75b25cb471dc9026693
saisharan <saisharangorantla@gmail.com>
2025-12-15 11:19:16 +0530

    initial commit

---
+++ Maven_projects/student_jdbc/src/main/java/com/skillnext1/App.java
@@ -0,0 +1,119 @@
+package com.skillnext1;
+
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Scanner sc=new Scanner(System.in);
+        StudentDao sd=new StudentDao();
+        while(true)
+        {
+            System.out.println("Welcome to Student Management System");
+            System.out.println("1. Add Student");
+            System.out.println("2. View Students");
+            System.out.println("3. Delete Student");
+            System.out.println("4. Update Student");
+            System.out.println("5. Exit");
+            System.out.println("6.count student acording to branches");
+            System.out.println("Enter your choice:");
+
+            int choice=sc.nextInt();
+            switch(choice)
+            {   
+                case 1:
+                {
+                    System.out.println("Enter name");
+                    String name=sc.next();
+                    System.out.println("Enter email");
+                    String email=sc.next();
+                    System.out.println("Enter dept");
+                    String dept=sc.next();
+                    student stu=new student(name,email,dept);
+                    try{
+                        sd.addStudent(stu);
+                        System.out.println("Student added successfully");
+                    }catch(Exception e)
+                    {
+                        System.out.println(e);
+                    }
+                    break;
+                }
+                case 2:
+                {
+                    try{
+                        for(student s:sd.getAllStudents())
+                        {
+                            System.out.println(s.getId()+" "+s.getName()+" "+s.getEmail()+" "+s.getDept());
+                        }
+                    }catch(Exception e)
+                    {
+                        System.out.println(e);
+                    }
+                    break;
+                }
+                case 3:
+                {
+                    System.out.println("Enter student id to delete:");
+                    int id=sc.nextInt();
+                    try{
+                        if(sd.exists(id))
+                        {
+                            System.out.println("Student with id "+id+" does not exist");
+                            break;
+                        }
+                        sd.deleteStudent(id);
+                        System.out.println("Student deleted successfully");
+                    }catch(Exception e)
+                    {
+                        System.out.println(e);
+                    }
+                    break;
+                }
+                case 4:
+                {
+                    System.out.println("Enter id,name,email,dept to update:");
+                    int id=sc.nextInt();
+                    String name=sc.next();
+                    String email=sc.next();
+                    String dept=sc.next();
+                    student stu=new student(name,email,dept);
+                    stu.setId(id);
+                    try{
+                        sd.updateStudent(stu);
+                        System.out.println("Student updated successfully");
+                    }catch(Exception e)
+                    {
+                        System.out.println(e);
+                    }
+                    break;
+                }
+                case 5:
+                {
+                    System.out.println("Exiting...");
+                    System.exit(0);
+                }
+                case 6:
+                {
+                    try{
+                        sd.getStudentCountByDept();
+                    }catch(Exception e)
+                    {
+                        System.out.println(e);
+                    }
+                    break;
+                }
+                default:
+                {
+                    System.out.println("Invalid choice");
+                }
+        }
+    }
+        
+    }
+}
